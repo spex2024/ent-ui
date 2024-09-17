0ce4945c8c71fce512f72b716519ec395765846a
spex2024 <dercolbags@gmail.com>
2024-09-17 15:29:27 +0000

    v1.0.0.0

--- admin/src/middleware.js
+++ admin/src/middleware.js
@@ -1,27 +1,16 @@
 import { NextResponse } from 'next/server';
-import jwt from 'jsonwebtoken';
 
 export function middleware(req) {
     const token = req.cookies.get('token')?.value; // Use the Next.js cookies API
     const url = new URL(req.url);
 
     if (token) {
-        try {
-            // Decode and verify the token
-            const decodedToken = jwt.verify(token, process.env.JWT_SECRET); // Replace with your secret
-
-            // If the token is valid, redirect to home/dashboard if trying to access login/signup
-            if (url.pathname === '/login' || url.pathname === '/signup') {
-                return NextResponse.redirect(new URL('/', req.url));
-            }
-        } catch (err) {
-            // If token verification fails (expired or invalid), redirect to login
-            if (err.name === 'TokenExpiredError' || err.name === 'JsonWebTokenError') {
-                return NextResponse.redirect(new URL('/login', req.url));
-            }
+        // Redirect to home or dashboard if trying to access login or signup page while logged in
+        if (url.pathname === '/login' || url.pathname === '/signup') {
+            return NextResponse.redirect(new URL('/', req.url));
         }
     } else {
-        // Redirect to login if no token is present and trying to access protected routes
+        // Redirect to login if no token is present
         if (url.pathname !== '/login' && url.pathname !== '/signup') {
             return NextResponse.redirect(new URL('/login', req.url));
         }
