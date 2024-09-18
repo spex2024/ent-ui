d3d35828f62b64d9d0f369548cd19ec76253de1c
spex2024 <dercolbags@gmail.com>
2024-09-18 12:39:57 +0000

    v1.0.0.0

--- admin/src/app/(page)/enterprises/page.tsx
+++ admin/src/app/(page)/enterprises/page.tsx
@@ -2,9 +2,6 @@
 import React, { useEffect } from 'react';
 import useAgencyStore from "@/store/agency";
 import AgencyTable from "@/components/page-ui/agencies-table";
-import useAuthStore from "@/store/authenticate";
-import {useRouter} from "next/navigation";
-import {ScaleLoader} from "react-spinners";
 
 const Enterprise = () => {
     const { agencies, fetchAgencies } = useAgencyStore();
--- admin/src/store/agency.js
+++ admin/src/store/agency.js
@@ -14,49 +14,17 @@ const useAgencyStore = create((set) => ({
     fetchAgencies: async () => {
         set({ loading: true, error: null });
         try {
-            const response = await axios.get(`${baseurl}/api/enterprise/agencies`, { withCredentials: true });
+            const response = await axios.get(`${baseurl}/api/admin/agency`, { withCredentials: true });
             set({ agencies: response.data, loading: false });
         } catch (error) {
             set({ error: error.message, loading: false });
         }
     },
 
-    updateAgency: async (agencyId, updatedData) => {
-        set({ loading: true, error: null });
-        try {
-            const response = await axios.patch(`${baseURL}/api/enterprise/agencies/${agencyId}`, updatedData, { withCredentials: true });
-            if (response.status === 200) {
-                set((state) => ({
-                    agencies: state.agencies.map(agency =>
-                        agency._id === agencyId ? { ...agency, ...updatedData } : agency
-                    ),
-                    loading: false,
-                }));
-            }
-        } catch (error) {
-            set({ error: error.message, loading: false });
-        }
-    },
 
     setError: (error) => set({ error }),
 }));
 
-export const getAllOrders = async (req, res) => {
-    try {
-        // Optionally, you can add filters based on query parameters
-        // const { status, userId } = req.query;
 
-        // Fetch all orders from the database
-        const orders = await Order.find()
-            .populate('user')  // Assuming you want to populate user details
-            .populate('vendor')  // Assuming you want to populate vendor details
-            .sort({ createdAt: -1 });  // Sort orders by creation date, latest first
-
-        res.status(200).json(orders);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: error.message });
-    }
-};
 
 export default useAgencyStore;
