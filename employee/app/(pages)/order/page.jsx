cb370a7cb7bde52e14cd8f37f8c846de5d20bca5
spex2024 <dercolbags@gmail.com>
2024-09-18 10:00:53 +0000

    v1.0.0.0

--- employee/app/(pages)/order/page.jsx
+++ employee/app/(pages)/order/page.jsx
@@ -1,35 +1,8 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { ScaleLoader } from "react-spinners";
-
 import OrderTable from "../../../components/page-ui/order-table";
-import useAuthStore from "../../store/authenticate";
 
 const Order = () => {
-  const { isAuthenticated } = useAuthStore();
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (!isAuthenticated) {
-        router.push("/login"); // Redirect to login page if not authenticated
-      }
-    }, 1000); // Adjust the delay as needed
-
-    return () => clearTimeout(timer); // Clean up the timer if the component unmounts
-  }, [isAuthenticated, router]);
-
-  // Optionally, you can return a loading indicator while checking authentication
-  if (!isAuthenticated) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <ScaleLoader color={"#000"} />
-      </div>
-    );
-  }
-
   return (
     <div className="lg:w-[80vw] lg:p-10 p-5 mx-auto min-h-screen ">
       <div className="flex flex-col lg:mb-10 p-5 gap-2">
