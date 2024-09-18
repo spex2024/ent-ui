2377d29ce5e03d6ff812457dfb51e8d87cabfdc0
spex2024 <dercolbags@gmail.com>
2024-09-18 10:18:36 +0000

    v1.0.0.0

--- employee/components/page-ui/not-found.jsx
+++ employee/components/page-ui/not-found.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const NotFound = ({ message }) => {
   return (
     <div className=" w-full flex flex-col items-center justify-center h-screen">
-      <p className="text-xl mt-4">{message || "No content found."}</p>
+      <p className="text-xl mt-4 text-center">{message || "No content found."}</p>
     </div>
   );
 };
